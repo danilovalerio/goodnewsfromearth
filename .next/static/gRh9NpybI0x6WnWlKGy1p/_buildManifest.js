86ddaa8709e91e37d024d76b20f07adc1b83d282
danilovalerio <danilo_vs@hotmail.com>
2023-11-13 11:06:15 -0300

    run build

---
+++ .next/static/gRh9NpybI0x6WnWlKGy1p/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-9911600be346eeaa.js"],"/_error":["static/chunks/pages/_error-dfcfa5bb62767c20.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
