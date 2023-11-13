835043d8f1f8419dd0363f1eb9eebe41cb99d7f0
danilovalerio <danilo_vs@hotmail.com>
2023-11-13 13:52:59 -0300

    prettier configurado

--- pages/index.js
+++ pages/index.js
@@ -2,5 +2,9 @@ function Home() {
   return <h1>Teste 2</h1>;
 }
 
+function teste() {
+  console.log("teste de msg");
+}
+
 //Export default determina a função padrao a ser chamada pelo next.js
 export default Home;
